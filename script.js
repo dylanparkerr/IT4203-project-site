fe8cd42ff2b7154ca0e9cac2a5e8422f750d3d81
Dylan Parker <dylanparkerr@gmail.com>
2020-09-18 18:13:48 -0400

    Fetching book JSON data

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 console.log("test");
 
-jQuery(document).ready(function(){
-    alert("jQuery works");
+$.getJSON("http://it-ebooks-api.info/v1/search/modern%20web", function(data){
+    console.log(data);
+});
+
+$.getJSON("http://it-ebooks-api.info/v1/book/2563063616", function(data){
+    console.log(data);
+});
+
+$.getJSON("https://www.googleapis.com/books/v1/volumes/Wfan6L9RGgYC", function(data){
+    console.log(data);
+});
+
+$.getJSON("https://www.googleapis.com/books/v1/volumes?q=modern%20web", function(data){
+    console.log(data);
 });
