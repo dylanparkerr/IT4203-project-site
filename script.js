8df090f036bcfada663f386f1549022d899ae99d
Dylan Parker <dylanparkerr@gmail.com>
2020-09-19 12:49:08 -0400

    Added local JSON resources

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 console.log("test");
 
-$.getJSON("https://api.itbook.store/1.0/books/9781449334970", function(data){
+$.getJSON("books/it-books-single.json", function(data){
     console.log(data);
 });
 
-$.getJSON("https://api.itbook.store/1.0/search/mobile%20web", function(data){
+$.getJSON("books/it-books-list.json", function(data){
     console.log(data);
 });
 
