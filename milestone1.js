a250b6dfca9f33d3c30c777fb67f317ec3ba90e9
Dylan Parker <dylanparkerr@gmail.com>
2020-09-20 18:45:02 -0400

    Completed source 4

--- milestone1.js
+++ milestone1.js
@@ -99,19 +99,70 @@ $.getJSON("https://www.googleapis.com/books/v1/volumes/Wfan6L9RGgYC", function(d
 
 $.getJSON("https://www.googleapis.com/books/v1/volumes?q=modern%20web", function(data){
     console.log(data);
+
+    $("[id=g0img]").attr("src",data.items[0].volumeInfo.imageLinks.smallThumbnail);
+    $("[id=g0title]").html(data.items[0].volumeInfo.title);
+    $("[id=g0subtitle]").html(data.items[0].volumeInfo.subtitle);
+    $("[id=g0isbn13]").html(data.items[0].volumeInfo.industryIdentifiers[0].identifier);
+    $("[id=g0isbn10]").html(data.items[0].volumeInfo.industryIdentifiers[1].identifier);
+
+    $("[id=g1img]").attr("src",data.items[1].volumeInfo.imageLinks.smallThumbnail);
+    $("[id=g1title]").html(data.items[1].volumeInfo.title);
+    $("[id=g1subtitle]").html(data.items[1].volumeInfo.subtitle);
+    $("[id=g1isbn13]").html(data.items[1].volumeInfo.industryIdentifiers[0].identifier);
+    $("[id=g1isbn10]").html(data.items[1].volumeInfo.industryIdentifiers[1].identifier);
+
+    $("[id=g2img]").attr("src",data.items[2].volumeInfo.imageLinks.smallThumbnail);
+    $("[id=g2title]").html(data.items[2].volumeInfo.title);
+    $("[id=g2subtitle]").html(data.items[2].volumeInfo.subtitle);
+    $("[id=g2isbn13]").html(data.items[2].volumeInfo.industryIdentifiers[0].identifier);
+    $("[id=g2isbn10]").html(data.items[2].volumeInfo.industryIdentifiers[1].identifier);
+
+    $("[id=g3img]").attr("src",data.items[3].volumeInfo.imageLinks.smallThumbnail);
+    $("[id=g3title]").html(data.items[3].volumeInfo.title);
+    $("[id=g3subtitle]").html(data.items[3].volumeInfo.subtitle);
+    $("[id=g3isbn13]").html(data.items[3].volumeInfo.industryIdentifiers[0].identifier);
+    $("[id=g3isbn10]").html(data.items[3].volumeInfo.industryIdentifiers[1].identifier);
+
+    $("[id=g4img]").attr("src",data.items[4].volumeInfo.imageLinks.smallThumbnail);
+    $("[id=g4title]").html(data.items[4].volumeInfo.title);
+    $("[id=g4subtitle]").html(data.items[4].volumeInfo.subtitle);
+    $("[id=g4isbn13]").html(data.items[4].volumeInfo.industryIdentifiers[0].identifier);
+    $("[id=g4isbn10]").html(data.items[4].volumeInfo.industryIdentifiers[1].identifier);
+
+    $("[id=g5img]").attr("src",data.items[5].volumeInfo.imageLinks.smallThumbnail);
+    $("[id=g5title]").html(data.items[5].volumeInfo.title);
+    $("[id=g5subtitle]").html(data.items[5].volumeInfo.subtitle);
+    $("[id=g5isbn13]").html(data.items[5].volumeInfo.industryIdentifiers[0].identifier);
+    $("[id=g5isbn10]").html(data.items[5].volumeInfo.industryIdentifiers[1].identifier);
+
+    $("[id=g6img]").attr("src",data.items[6].volumeInfo.imageLinks.smallThumbnail);
+    $("[id=g6title]").html(data.items[6].volumeInfo.title);
+    $("[id=g6subtitle]").html(data.items[6].volumeInfo.subtitle);
+    $("[id=g6isbn13]").html(data.items[6].volumeInfo.industryIdentifiers[0].identifier);
+    $("[id=g6isbn10]").html(data.items[6].volumeInfo.industryIdentifiers[1].identifier);
+
+    $("[id=g7img]").attr("src",data.items[7].volumeInfo.imageLinks.smallThumbnail);
+    $("[id=g7title]").html(data.items[7].volumeInfo.title);
+    $("[id=g7subtitle]").html(data.items[7].volumeInfo.subtitle);
+    $("[id=g7isbn13]").html(data.items[7].volumeInfo.industryIdentifiers[0].identifier);
+    $("[id=g7isbn10]").html(data.items[7].volumeInfo.industryIdentifiers[1].identifier);
+
+    $("[id=g8img]").attr("src",data.items[8].volumeInfo.imageLinks.smallThumbnail);
+    $("[id=g8title]").html(data.items[8].volumeInfo.title);
+    $("[id=g8subtitle]").html(data.items[8].volumeInfo.subtitle);
+    $("[id=g8isbn13]").html(data.items[8].volumeInfo.industryIdentifiers[0].identifier);
+    $("[id=g8isbn10]").html(data.items[8].volumeInfo.industryIdentifiers[1].identifier);
+
+    $("[id=g9img]").attr("src",data.items[9].volumeInfo.imageLinks.smallThumbnail);
+    $("[id=g9title]").html(data.items[9].volumeInfo.title);
+    $("[id=g9subtitle]").html(data.items[9].volumeInfo.subtitle);
+    $("[id=g9isbn13]").html(data.items[9].volumeInfo.industryIdentifiers[0].identifier);
+    $("[id=g9isbn10]").html(data.items[9].volumeInfo.industryIdentifiers[1].identifier);
+
 });
 
 hideAll();
-
-
-
-
-
-
-
-
-
-
 
 function show(){ 
     option = $('[name=source-list]').val();
