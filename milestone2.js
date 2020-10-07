69b29f613bf5a5863c5ea13eeeaf288e5892159c
Dylan Parker <dylanparkerr@gmail.com>
2020-10-07 14:42:34 -0400

    Initial setup of dynamically changing content

--- milestone2.js
+++ milestone2.js
@@ -1,8 +1,17 @@
 console.log("milestone2");
+function hideAll() {
+    $("[id=bookList]").hide();
+    $("[id=bookDetails]").hide();
+    $("[id=bookshelf]").hide();
+    $("[id=backBtn]").hide();
+    $("[id=pageNumberRow]").hide();
+}
+hideAll();
 
 function formSearchURL(searchTerms) {
     const URL_FRONT = "https://www.googleapis.com/books/v1/volumes?q=";
-    const URL_BACK = "&key=AIzaSyAapv3F22n1UHUtHh5bnUKM3vHm62bfvXg";
+    const URL_BACK =
+        "&maxResults=40&key=AIzaSyAapv3F22n1UHUtHh5bnUKM3vHm62bfvXg";
 
     const terms = searchTerms.split(" ");
 
@@ -15,10 +24,33 @@ function formSearchURL(searchTerms) {
         }
     }
     url += URL_BACK;
+    console.log(url);
     return url;
 }
 
 function search() {
     const searchTerms = $("[id=searchInput]").val();
     const url = formSearchURL(searchTerms);
+    $.get(url, function (data) {
+        console.log(data);
+    });
+    showList();
+}
+
+function showList() {
+    hideAll();
+    $("[id=bookList]").show();
+    $("[id=pageNumberRow]").show();
+}
+
+function showDetails() {
+    hideAll();
+    $("[id=bookDetails]").show();
+    $("[id=backBtn]").show();
+}
+
+function showBookshelf() {
+    hideAll();
+    $("[id=bookshelf]").show();
+    $("[id=backBtn]").show();
 }
